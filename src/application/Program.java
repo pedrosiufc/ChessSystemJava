c7a955c7faaa20e37b162afb75b31ca9805eec13
pedrosiufc <pedrosiufc@gmail.com>
2024-07-23 08:59:22 -0300

    test token

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("test commit");
+		System.out.println("test token");
 	}
 
 }
